4798dd2374b7ef55da44b2bab58e21790328e0a8
Moutasim02 <moutasim9997@gmail.com>
2023-02-27 01:05:29 +0400

    code CC01 first 4 done.

---
+++ CC01/ex05/ft_putstr.c
@@ -0,0 +1,21 @@
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str) {
+    while (str != NULL)
+    {
+        // c *str;
+
+    }
+    
+}
+
+int main()
+{
+    ft_putstr("Moutasim");
+    return 0;
+}
